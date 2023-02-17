a5c6a1a52f54bc0e8aae6057bb86dbe7b01ad4f6
Caroline Syokau <caroline.syokau@student.moringaschool.com>
2023-02-17 12:13:23 +0300

    completed assignment

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,34 @@
 class Triangle
   # write code here
+  
 end
+  attr_reader :s1, s2, s3
+  def initialize(s1, s2, s3)
+    @s1 = s1
+    @s2 = s2
+    @s3 = s3
+  end
+  def kind
+    validate_triangle
+    if s1 == s2 && s2 = s3
+      :equilateral
+    elseif s1 == s2 || s2 == s3 || s1 == s3
+      :isosceles
+    else
+      :scalene
+    end
+
+   end
+   def sides > 0?
+    [s1, s2, s3].all? (&:positive)
+   end
+   def valid_triangle_inequlity?
+    s1 + s2 > s3 && s1 + s3 > s2 && s2 + s3 > s1
+    end
+    def validate_triangle
+      raise TriangleError unless sides > 0? && valid_triangle_inequlity?
+    end
+    class TriangleError < StandardError
+    end
+
+  end
